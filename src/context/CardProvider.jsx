56a5edbcb29c486c0ffb58ffb46cb874b294916f
jorgelol20 <94186347+jorgelol20@users.noreply.github.com>
2026-08-12 13:34:58 +0200

    arreglar error build

--- src/context/CardProvider.jsx
+++ src/context/CardProvider.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment, createContext, useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import usePokeAPI from "./../hooks/usePokeAPI.js";
+import usePokeAPI from "../hooks/usePokeAPI.js";
 import { errorContext } from "./ErrorProvider.jsx";
 import { parseDeckList } from "./../utils/parseDeckList.jsx";
 import { useContext } from "react";
