942a4807801af206041e09f93964de402c6b21ff
Jorge Colomer Albertos <94186347+jorgelol20@users.noreply.github.com>
2026-08-13 20:49:28 +0200

    Arreglos (#25)
    * Añadir vercel.json
    
    * Arreglos (#26)
    
    * Mejoras del constructor de mazos
    
    * arreglar error build
    
    * arreglar error build
    
    * arreglar error build
    
    * Mejoras del constructor de mazos
    
    * arreglar error build
    
    * arreglar error build
    
    * Fix/readme and button (#15)
    
    * Arreglar readme y botón
    
    * Areglar error tonto
    
    * Mejoras en la importación de cartas (#17)
    
    * Feature/mejoras importaciones (#19)
    
    * Mejoras en la importación de cartas
    
    * Mejoras en idioma
    
    * Feature/mejoras importaciones (#21)
    
    * Mejoras en la importación de cartas
    
    * Mejoras en idioma
    
    * Arreglado el modal
    
    * Añadir vercel.json (#23)
    
    * Arreglos (#25)
    
    * Añadir vercel.json
    
    * Arreglos

--- src/components/DecksList.jsx
+++ src/components/DecksList.jsx
@@ -31,16 +31,6 @@ const DecksList = () => {
         <Fragment>
             <div>
                 <h1>Listado de mazos</h1>
-                <div className='adviceBox'>
-                    {
-                        i18n.language == "es" && <Advice text={"Algunas expansiones no están disponibles aún en español. Las cartas en el formato estándard funcionan sin ningún problema. Se recomienda el uso del idioma inglés si vas a hacer uso de cartas del formato 'Expandido'. Disculpar las molestias"} type={"importante"} />
-                    }
-                </div>
-                <div className="error">
-                    {
-                        error && moveToError() && <ErrorAlert id="error" errorMessage={contextError} />
-                    }
-                </div>
                 <div className="deck-list">
                     {
                         decksList !== undefined && decksList.length > 0 ? decksList?.map((deck) => {
